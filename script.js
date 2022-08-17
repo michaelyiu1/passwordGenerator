204a7877f878d233a4cfa4199f0f5d1f6420602b
Michael Yiu <maplepie32@gmail.com>
2022-08-16 17:22:12 -0700

    added user criteria for password in js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +13,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// This function will ask the user what criteria they want to include and then generate the password
+function generatePassword(){
+    let lowCase = confirm("Do you want lower cases in your password?")
+    console.log(lowCase);
+
+    let upCase = confirm("Do you want upper cases in your password?");
+    console.log(upCase);
+
+    let numInclude = confirm("Do you want to include numbers in your password?");
+    
+    let specialChar = confirm("Do you want to include special characters in your password?");
+    console.log(specialChar);
+        
+    let passLength =  prompt("How many characters do you want your password to be? (Pick a number between 8-128");
+
+    if (passLength>=8 && passLength<=128){
+        console.log(passLength)
+    } else {
+        alert("Not valid. Please choose a number between 8-128");
+    }
+
+    
+
+
+}
