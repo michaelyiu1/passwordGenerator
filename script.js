b91f98ec86549d8d6d0dc5433aa8349ae0bbd5f0
Michael Yiu <maplepie32@gmail.com>
2022-08-17 12:54:15 -0700

    completed function to generate password

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ generateBtn.addEventListener("click", writePassword);
 
 // This function will ask the user what criteria they want to include and then generate the password
 function generatePassword(){
+    
+    //User Questions
     let lowCase = confirm("Do you want lower cases in your password?")
     console.log(lowCase);
 
@@ -30,13 +32,57 @@ function generatePassword(){
         
     let passLength =  prompt("How many characters do you want your password to be? (Pick a number between 8-128");
 
+    //Alert when user inputs an invalid number
     if (passLength>=8 && passLength<=128){
         console.log(passLength)
     } else {
         alert("Not valid. Please choose a number between 8-128");
     }
 
+    //Lowercase and Uppercase letter generation
+    if (lowCase && upCase ) {
+        var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else if (lowCase === true && upCase === false){
+        var letters = "abcdefghijklmnopqrstuvwxyz";
+    } else if (lowCase === false && upCase === true){
+        var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else {
+        var letters = "";
+    }
+    console.log(letters);
+
+    //Adding numbers to string
+    if (numInclude){
+        var numbers = "0123456789";
     
+    } else {
+        var numbers = "";
+    } 
+
+  
+    let characters = letters.concat(numbers);
 
 
+    //adding special characters to string
+    if (specialChar){
+        specialstring = "~!@#$%^&*";
+    } else {
+        specialstring = "";
+    }
+
+    let charFinal = characters.concat(specialstring);
+    console.log(charFinal);
+
+
+    //initalize password
+    var finalPassword = "";
+    //For loop for generating password
+    for (i=0; i<passLength; i++){
+        randNum = Math.floor(Math.random() * passLength);
+        randChar = charFinal[randNum];
+        console.log(randChar)
+        var finalPassword = finalPassword.concat(randChar);
+    }
+
+return finalPassword;
 }
